6d0243b439fb4bd5ee9f23bd77d510176f2e2972
imjabed <contactabujabed@gmail.com>
2025-06-09 00:58:33 +0530

    cors allowed

--- backend/api/send-email.js
+++ backend/api/send-email.js
@@ -1,7 +1,14 @@
-
 const nodemailer = require('nodemailer');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://imabujabed.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).send({ message: 'Only POST requests allowed' });
   }
